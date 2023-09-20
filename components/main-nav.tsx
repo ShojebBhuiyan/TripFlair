710bca51e506ad1184d85173b6bca2850824745d
ShojebBhuiyan <shojeb.bhuiyan@gmail.com>
2023-09-20 18:53:46 +0600

    Navbar

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -1,4 +1,5 @@
 import * as React from "react"
+import Image from "next/image"
 import Link from "next/link"
 
 import { NavItem } from "@/types/nav"
@@ -6,19 +7,20 @@ import { siteConfig } from "@/config/site"
 import { cn } from "@/lib/utils"
 import { Icons } from "@/components/icons"
 
+import { Button } from "./ui/button"
+
 interface MainNavProps {
   items?: NavItem[]
 }
 
 export function MainNav({ items }: MainNavProps) {
   return (
-    <div className="flex gap-6 md:gap-10">
+    <div className="flex w-full gap-6 md:gap-10">
       <Link href="/" className="flex items-center space-x-2">
-        <Icons.logo className="h-6 w-6" />
-        <span className="inline-block font-bold">{siteConfig.name}</span>
+        <Image src="/logo.png" width={128} height={128} alt="logo" />
       </Link>
       {items?.length ? (
-        <nav className="flex gap-6">
+        <nav className="flex w-full justify-center gap-6">
           {items?.map(
             (item, index) =>
               item.href && (
@@ -36,6 +38,9 @@ export function MainNav({ items }: MainNavProps) {
           )}
         </nav>
       ) : null}
+      <nav className="flex items-center justify-center gap-6">
+        <Button variant={"link"}>Login</Button>
+      </nav>
     </div>
   )
 }
--- config/site.ts
+++ config/site.ts
@@ -1,18 +1,17 @@
 export type SiteConfig = typeof siteConfig
 
 export const siteConfig = {
-  name: "Next.js",
-  description:
-    "Beautifully designed components built with Radix UI and Tailwind CSS.",
+  name: "TripFlair",
+  description: "Not All Who Wander Are Lost",
   mainNav: [
     {
       title: "Home",
       href: "/",
     },
+    {
+      title: "About Us",
+      href: "/about",
+    },
   ],
-  links: {
-    twitter: "https://twitter.com/shadcn",
-    github: "https://github.com/shadcn/ui",
-    docs: "https://ui.shadcn.com",
-  },
+  links: {},
 }
