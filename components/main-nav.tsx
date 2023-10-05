b867399a43ca222a81f06f7446799a5775285d65
ShojebBhuiyan <shojeb.bhuiyan@gmail.com>
2023-10-05 11:23:02 +0600

    Logo resize

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -16,7 +16,7 @@ export function MainNav({ items }: MainNavProps) {
   return (
     <div className="flex w-full gap-6 md:gap-10">
       <Link href="/" className="flex items-center space-x-2">
-        <Image src="/logo.png" width={128} height={128} alt="logo" />
+        <Image src="/logo.png" width={256} height={256} alt="logo" />
       </Link>
       {items?.length ? (
         <nav className="flex w-full justify-center gap-32">
