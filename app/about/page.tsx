654b23a5b2ff244ae0e4c15f2ed8a5c82b48d198
Md. Shahriar Islam Bhuiyan <shojeb.bhuiyan@gmail.com>
2023-11-29 12:07:44 +0600

    About page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,7 +1,100 @@
 export default function AboutPage() {
   return (
-    <div className="flex h-[70vh] flex-col items-center justify-center gap-5">
-      asdsad
+    <div className="flex flex-col">
+      <section className="flex h-screen flex-col items-center justify-center gap-6 bg-landing bg-cover bg-no-repeat">
+        <h1 className="text-7xl font-bold text-white">Not All Who Wander</h1>
+        <h1 className="text-7xl font-bold text-white">Are Lost </h1>
+      </section>
+      <section className="bg-[#F2F6F6] bg-worldMap bg-cover bg-no-repeat py-20">
+        <div className="container flex flex-col justify-center gap-6 bg-worldMap bg-cover bg-no-repeat">
+          <p className="text-3xl text-black">
+            Welcome to TripFlair! Your passport to unparalleled adventures and
+            effortless travel planning. At TripFlair, we believe in transforming
+            your wanderlust into cherished memories, offering a seamless
+            platform to curate your own stories of exploration and delight.
+          </p>
+          <h1 className="text-3xl font-bold text-black">Who We Are:</h1>
+          <p className="text-2xl text-black">
+            TripFlair isn&apos;t just a tour planning website; we&apos;re your
+            dedicated travel companions, committed to turning your travel dreams
+            into reality. Fueled by a passion for discovery and a dedication to
+            exceptional service, we redefine your travel experience.
+          </p>
+          <h1 className="text-4xl font-bold text-black">Our Mission:</h1>
+          <p className="text-2xl text-black">
+            At TripFlair, our mission is clear – to inspire and empower you to
+            embark on journeys that leave you enriched, enlightened, and eager
+            for more. We&apos;re devoted to crafting exceptional travel
+            experiences tailored to your unique interests, preferences, and
+            aspirations.
+          </p>
+          <h1 className="text-4xl font-bold text-black">What Sets Us Apart:</h1>
+          <p className="text-2xl text-black">
+            Personalized Planning: Recognizing that each traveler is unique, we
+            take the time to understand your preferences, ensuring that every
+            itinerary is a reflection of your desires.
+          </p>
+          <p className="text-2xl text-black">
+            <span className="underline">Expert Guidance:</span> Our team of
+            experienced travel enthusiasts and experts is here to guide you at
+            every step. From choosing the perfect destination to recommending
+            hidden gems, we&apos;re dedicated to making your adventure
+            stress-free.
+          </p>
+          <p className="text-2xl text-black">
+            <span className="underline">Quality Partnerships:</span> TripFlair
+            collaborates with trusted partners worldwide, bringing you the best
+            in accommodations, transportation, and activities. Our carefully
+            curated network guarantees that you experience the finest aspects of
+            every destination.
+          </p>
+          <p className="text-2xl text-black">
+            <span className="underline">Innovative Technology:</span> Combining
+            cutting-edge technology with a personalized touch, TripFlair&apos;s
+            user-friendly platform allows you to seamlessly plan and book your
+            trips. Our support team is always ready to assist with any
+            inquiries.
+          </p>
+          <p className="text-2xl text-black">
+            <span className="underline">Sustainable Travel:</span> At TripFlair,
+            we embrace responsible tourism. Our commitment to sustainable travel
+            practices ensures that your journeys not only benefit you but also
+            contribute positively to the destinations you explore.
+          </p>
+          <p className="text-2xl text-black">
+            Embark on a journey beyond the ordinary with TripFlair. Let us be
+            your compass, guiding you to extraordinary destinations and
+            unforgettable memories. Your adventure begins here.
+          </p>
+        </div>
+      </section>
     </div>
   );
 }
+
+// Welcome to TripFlair! Your passport to unparalleled adventures and effortless travel planning. At TripFlair, we believe in transforming your wanderlust into cherished memories, offering a seamless platform to curate your own stories of exploration and delight.
+
+// Who We Are:
+// TripFlair isn't just a tour planning website; we're your dedicated travel companions, committed to turning your travel dreams into reality. Fueled by a passion for discovery and a dedication to exceptional service, we redefine your travel experience.
+
+// Our Mission:
+// At TripFlair, our mission is clear – to inspire and empower you to embark on journeys that leave you enriched, enlightened, and eager for more. We're devoted to crafting exceptional travel experiences tailored to your unique interests, preferences, and aspirations.
+
+// What Sets Us Apart:
+
+// Personalized Planning:
+// Recognizing that each traveler is unique, we take the time to understand your preferences, ensuring that every itinerary is a reflection of your desires.
+
+// Expert Guidance:
+// Our team of experienced travel enthusiasts and experts is here to guide you at every step. From choosing the perfect destination to recommending hidden gems, we're dedicated to making your adventure stress-free.
+
+// Quality Partnerships:
+// TripFlair collaborates with trusted partners worldwide, bringing you the best in accommodations, transportation, and activities. Our carefully curated network guarantees that you experience the finest aspects of every destination.
+
+// Innovative Technology:
+// Combining cutting-edge technology with a personalized touch, TripFlair's user-friendly platform allows you to seamlessly plan and book your trips. Our support team is always ready to assist with any inquiries.
+
+// Sustainable Travel:
+// At TripFlair, we embrace responsible tourism. Our commitment to sustainable travel practices ensures that your journeys not only benefit you but also contribute positively to the destinations you explore.
+
+// Embark on a journey beyond the ordinary with TripFlair. Let us be your compass, guiding you to extraordinary destinations and unforgettable memories. Your adventure begins here.
